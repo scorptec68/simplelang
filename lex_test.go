49d0c47d79d6b2779758613d591b40ec91b8441d
Tim Shimmin <timothy.shimmin@gmail.com>
2018-07-02 20:52:01 +1000

    Fix up example test.

--- lex_test.go
+++ lex_test.go
@@ -20,6 +20,23 @@ func ExampleLexing() {
 			break
 		}
 	}
-	// Output: woof
+	/* Output:
+item: <var>
+item: "\n" (type 19)
+item: "x" (type 21)
+item: ":" (type 18)
+item: <integer>
+item: "\n" (type 19)
+item: <endvar>
+item: "\n" (type 19)
+item: <run>
+item: "\n" (type 19)
+item: "x" (type 21)
+item: "=" (type 3)
+item: "3" (type 1)
+item: "\n" (type 19)
+item: <endrun>
+item: EOF
+	*/
 
 }
