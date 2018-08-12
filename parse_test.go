6435564ef7966e31cc1cadae5d839e1d37b3ebc3
scorptec68 <timothy.shimmin@gmail.com>
2018-08-12 22:06:06 +1000

    Pust in original example. TODO: need to still do integer comparisons!!! Almost there :-)

--- parse_test.go
+++ parse_test.go
@@ -3,25 +3,63 @@ package simple_language
 import "fmt"
 
 func ExampleParse1() {
+	inputStr := `var
+	   test: string
+	   x: integer
+	   y: integer
+	   i: integer
+	   doit: boolean
+	   this-boy: boolean
+	endvar
+	
+	run
+	    this-boy = true
+	    x = 3 + 1000000 + 2 +
+	          4 + 6
+	    x = 3
+		test = "hello"
+		    
+		y = x + 3
+		print("y =  " + strInt(x))
+		
+		if y > 3
+			test = "super"
+		elseif y < 3 
+		    test = "wonder"
+		else
+		    test = "duper"
+		endif
+		
+		print(test)
+		test = test + "r"
+		print(test)
+		
+		loop times 10
+		    i = i + 1
+		endloop
+		
+		print("i = " + strInt(i))
+	endrun
+	`
 
 	// test input
-	inputStr := `var
-       x: boolean
-       y: boolean
-	   z: boolean
-	   a: integer
-	   b: integer
-	   c: string
-    endvar
+	// inputStr := `var
+	//    x: boolean
+	//    y: boolean
+	//    z: boolean
+	//    a: integer
+	//    b: integer
+	//    c: string
+	// endvar
 
-    run
-       //x = true
-       //y = true | false
-	   //y = x & (b | false)
-       //z = y & false | x & (y | true)
-	   //z = y & false | x & y | true
-	   //z = (x | y & z) | (true & false | x)
-	//    if x & y 
+	// run
+	//x = true
+	//y = true | false
+	//y = x & (b | false)
+	//z = y & false | x & (y | true)
+	//z = y & false | x & y | true
+	//z = (x | y & z) | (true & false | x)
+	//    if x & y
 	// 	  z = y
 	// 	  z = true
 	//    elseif true
@@ -31,17 +69,17 @@ func ExampleParse1() {
 	//    else
 	// 	  z = false
 	//    endif
-	    // loop x & y
-		//    x = true
-		// endloop
-		//x = y | (z | true) & x
-		//a = 2 / 3
-		//a = 2 * 3
-		//a = 3 + (b / a + 2) * 6
-		//c = "hello" + " " + "there"
-		//c = strInt(2 + 3 / 5)
-		print("hi" + " " + "there")
-    endrun`
+	// loop x & y
+	//    x = true
+	// endloop
+	//x = y | (z | true) & x
+	//a = 2 / 3
+	//a = 2 * 3
+	//a = 3 + (b / a + 2) * 6
+	//c = "hello" + " " + "there"
+	//c = strInt(2 + 3 / 5)
+	//print("hi" + " " + "there")
+	//endrun`
 
 	l := lex("test", inputStr)
 	parser := NewParser(l)
